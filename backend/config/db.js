e9e02fc8a76c2f7d58f188fc5380953ef450dfd0
Sowparnika-13 <sowparnikag375@gmail.com>
2025-05-05 23:46:28 +0530

    Add files via upload

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,1 +1,20 @@
+const mysql = require('mysql2');
 
+// Create a connection to the MySQL database
+const db = mysql.createConnection({
+host: "localhost",
+user: "root",         // Change if your MySQL user is different
+password: "WJ28@krhps",         // Add your MySQL password here if required
+database: "college_portal"
+});
+
+// Connect to the database
+db.connect((err) => {
+if (err) {
+console.error("Database Connection Failed!", err.message);
+return;
+}
+console.log("Database Connected!");
+});
+
+module.exports = db;
